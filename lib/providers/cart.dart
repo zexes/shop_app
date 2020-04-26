9f703cd023a5079ada6fff9471b278a30bbb25b2
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-04-26 18:45:16 +0100

    Adding Shopping Cart data. closes #8

---
+++ lib/providers/cart.dart
@@ -0,0 +1,50 @@
+import 'dart:collection';
+import 'dart:html';
+
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return UnmodifiableMapView(_items);
+  }
+
+  void addItem({String id, double price, String title}) {
+    //no quantity cos we can only ass one cart item per time
+    if (_items.containsKey(id)) {
+      _items.update(
+          id,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                price: existingCartItem.price,
+                quantity: existingCartItem.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+        id,
+        () => CartItem(
+          id: id,
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+  }
+}
