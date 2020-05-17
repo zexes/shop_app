a6d975902e1ec3f2ea51ea5181befef0feda4b3a
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-05-17 13:20:13 +0100

    Image Input & Image Preview. closes #26

--- lib/screens/add_edit_product_screen.dart
+++ lib/screens/add_edit_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
@@ -10,12 +11,31 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlController.addListener(
+        _updateImageUrl); // auto fetch image when field loses focus
+  }
 
   @override
   void dispose() {
+    _imageUrlFocusNode
+        .removeListener(_updateImageUrl); // this before removing focus node
     _priceFocusNode.dispose(); // to prevent memory leakages
     _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocusNode.hasFocus) {
+      setState(() {});
+    }
   }
 
   @override
@@ -52,6 +72,38 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   keyboardType: TextInputType.multiline,
                   focusNode: _descriptionFocusNode,
                 ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      height: 100.0,
+                      margin: EdgeInsets.only(top: 8.0, right: 10.0),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        width: 1.0,
+                        color: Colors.grey,
+                      )),
+                      child: _imageUrlController.text.isEmpty
+                          ? Text('Enter a Url')
+                          : FadeInImage(
+                              placeholder:
+                                  AssetImage('assets/images/shopping.PNG'),
+                              image: NetworkImage(_imageUrlController.text),
+                              fit: BoxFit.cover,
+                            ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Image URL'),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        controller: _imageUrlController,
+                        focusNode: _imageUrlFocusNode,
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
           ),
