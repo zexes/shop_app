3d23c9a8d518ba871dd9bcf6fa3ad26760e2a8ac
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2020-06-06 09:34:06 +0100

    Using the "FutureBuilder" Widget. BEST TO MANAGE DATA LOADING AND WHEN DONE, rather than manually managing loading in STATEFUL WIDGET. closes #46

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,47 +4,38 @@ import '../widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 import '../providers/orders.dart' show Orders;
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const String id = 'orders_screen';
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    _isLoading = true;
-    Provider.of<Orders>(context, listen: false)
-        .fetchAndSetOrders()
-        .then((_) => setState(() {
-              _isLoading = false;
-            }));
-
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Orders'),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : Consumer<Orders>(
-              builder: (ctx, ordersData, Widget child) {
-                return ListView.builder(
-                  itemBuilder: (ctx, index) {
-                    return OrderItem(order: ordersData.orders[index]);
-                  },
-                  itemCount: ordersData.orders.length,
-                );
-              },
-            ),
-    );
+        appBar: AppBar(
+          title: Text('Your Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (dataSnapshot.error != null) {
+              //handle error
+              return Center(child: Text('An error occurred'));
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, ordersData, Widget child) {
+                  return ListView.builder(
+                    itemBuilder: (ctx, index) {
+                      return OrderItem(order: ordersData.orders[index]);
+                    },
+                    itemCount: ordersData.orders.length,
+                  );
+                },
+              );
+            }
+          },
+        ));
   }
 }
