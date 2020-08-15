dae4123857253ed16f6fe6209c44035c44724a6a
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-08-15 16:26:56 +0100

    Working with animated Container. closes #63

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -108,7 +108,7 @@ class _AuthCardState extends State<AuthCard>
   Animation<Size> _heightAnimation;
 
   @override
-  void initState() {
+  void initState() {//not  needed when using animatedContainer
    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 300,),);
    _heightAnimation = Tween<Size>(begin: Size(double.infinity, 260), end: Size(double.infinity, 320),).animate(CurvedAnimation(parent: _controller, curve: Curves.bounceInOut),);
 //   _heightAnimation.addListener(() => setState((){}));
@@ -206,14 +206,16 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(animation: _heightAnimation, builder: (ctx, ch) => Container(
-//        height: _authMode == AuthMode.SignUp ? 320 : 260,
-        height: _heightAnimation.value.height,
+      child:AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+        height: _authMode == AuthMode.SignUp ? 320 : 260,
+//        height: _heightAnimation.value.height,
         constraints:
-        BoxConstraints(minHeight: _heightAnimation.value.height),
+        BoxConstraints(minHeight: _authMode == AuthMode.SignUp ? 320 : 260),
         width: deviceSize.width * 0.75,
         padding: EdgeInsets.all(16.0),
-        child: ch), child: Form(
+        child:Form(
         key: _formKey,
         child: SingleChildScrollView(
           child: Column(
